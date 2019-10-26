0012fe6b2ecaa511538497aeef8d369ea3167f98
Markus Sveggen <marks18@uia.no>
2019-10-27 00:08:27 +0200

    Ferdigstilte email-klasser for merge

--- VikingAirways/src/classes/BookingNumberEmail.java
+++ VikingAirways/src/classes/BookingNumberEmail.java
@@ -1,53 +1,40 @@
 package classes;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BookingNumberEmail {
-    Statement stmnt;
+    public void sendEmail(Connection conn, String bookingNumber)  {
+            String strSelect = "SELECT * FROM Customer, Flight, Booking WHERE Booking.customer_id_fk = Customer.customer_id " +
+                    "AND Booking.flight_number_fk = Flight.flight_number AND Booking.booking_number= "+bookingNumber;
 
-
-    public static void main(String[] args){
-        BookingNumberEmail bn = new BookingNumberEmail();
-        bn.sendEmail("3");
-
-    }
-    public void sendEmail(String bookingNumber) {
-            //Koble til database
-            Connection conn;
-            DBConnect dbconnect = new DBConnect();
-            conn = dbconnect.connectToDB();
-
-            String strSelect = "SELECT * FROM Customer WHERE customer_id = " + bookingNumber;
             try {
-                stmnt = conn.createStatement();
+                Statement stmnt = conn.createStatement();
                 ResultSet rset = stmnt.executeQuery(strSelect);
 
-                if (rset.next()) {
+                while (rset.next()) {
+                    //Creates variables from values collected from Database
                     String firstName = rset.getString("first_name");
                     String lastName = rset.getString("last_name");
-                    String departureAirport = "";
-                    String arrivalAirport = "";
+                    String departureAirport = rset.getString("departure_airport");
+                    String arrivalAirport = rset.getString("arrival_airport");
+                    String departure_date = rset.getString("departure_date");
 
-                    //Declare variables to be used when sending an email.
-                    String recipient = "m.sveggen@gmail.com";
+                    //Creates variables to be used when sending an email.
+                    String recipient = rset.getString("email");
                     String subject = "Booking confirmation";
                     String content = "Hello " + firstName + " " + lastName + ". <br>" +
                             "Your bookingnumber is " + bookingNumber + ", for your flight from " + departureAirport +
-                            " to " + arrivalAirport + "." + "<br><br>" +
+                            " to " + arrivalAirport + " on the "+ departure_date + "."+"<br><br>" +
                             "Have a nice flight! <br>" +
                             "<b>Viking Airways</b> <br>" +
                             "NO-4635 Kristiansand, Norway <br>" +
                             "Tel. +47 38 04 55 38 <br>";
 
-                    //Creates an email object.
+                    //Creates an email object and sends the email to the recipient.
                     Email email = new Email();
                     email.sendEmail(recipient, subject, content);
-
-                } else {
-                    System.out.println("Email was not sent");
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
---
+++ VikingAirways/src/classes/Main.java
@@ -0,0 +1,13 @@
+package classes;
+
+import javax.naming.NamingException;
+
+public class Main {
+    public static void main(String[] args) {
+        BookingNumberEmail bn = new BookingNumberEmail();
+       // bn.sendEmail1("3");
+
+    }
+
+
+}
