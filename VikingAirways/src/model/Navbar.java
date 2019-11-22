24d56eff0c964766aca96840dd124dbca94c2311
Markus Sveggen <marks18@uia.no>
2019-11-22 01:55:27 +0100

    endret på navbar-klassen

--- VikingAirways/src/model/Navbar.java
+++ VikingAirways/src/model/Navbar.java
@@ -19,7 +19,7 @@ public class Navbar {
         out.println("  </ul>");
         out.println("  <ul class=\"navbar-nav ml-auto\">");
         out.println("    <li class=\"nav-item\">");
-        out.println("      <a class=\"nav-link\" href=\"register.jsp\">controller.Register</a>");
+        out.println("      <a class=\"nav-link\" href=\"register.jsp\">Register</a>");
         out.println("    </li>");
         out.println("    <li class=\"nav-item\">");
         out.println("      <a class=\"nav-link\" href=\"login.jsp\">Log In</a>");
