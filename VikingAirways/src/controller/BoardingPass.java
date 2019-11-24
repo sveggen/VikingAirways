49a947d17dfad9c1f98f895a0deb0f280e028206
jorgenl95 <jorgel18@uia.no>
2019-11-24 15:03:20 +0100

    Diverse

--- VikingAirways/src/controller/BoardingPass.java
+++ VikingAirways/src/controller/BoardingPass.java
@@ -14,18 +14,37 @@ import com.itextpdf.text.*;
 
 import model.*;
 
+/**
+ * This servlet handles a request from checkIn.jsp.
+ * It gets the booking number entered, sets the path where the PDF should be stored
+ * and passes this as parameters to BoardingPassPDF to generated the PDF.
+ * Finally it gets the name of the PDF and forwarrds this to showBoardingPass.jsp
+ *
+ * @author Jørgen Lindbøl
+ * @version 24.11.2019
+ */
 @WebServlet(name = "BoardingPass", urlPatterns = {"/BoardingPass"})
 public class BoardingPass extends HttpServlet {
 
+    /**
+     * Standard Servlet method to handle a post request.
+     * Gets the booking number passed through the request object,
+     * gets the path to PDF storage location and calls on BoardingPassPDF
+     * to generated the PDF. Forwards to showBoardingPass.jsp.
+     *
+     * @param request Request object received from user, currently checkIn.jsp
+     * @param response Response object sent back to the user, currently showBoardingPass.jsp
+     * @throws IOException Thrown if an I/O exception of some sort has occurred
+     * @throws ServletException Thrown if exceptions related to calling the servlet occur
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        PrintWriter out = response.getWriter();
+
         String fileName;
 
         String bookingNumber = request.getParameter("bookingNumber");
 
         String tomcatPath = request.getServletContext().getRealPath("/boardingpasses");
 
-        //out.println("Your booking number is: " + bookingNumber);
 
         BoardingPassPDF pdffunc = new BoardingPassPDF();
         pdffunc.makePDF(bookingNumber, tomcatPath);
@@ -37,6 +56,7 @@ public class BoardingPass extends HttpServlet {
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
         dispatcher.forward(request,response);
     }
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     }
